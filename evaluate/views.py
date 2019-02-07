c65c8f4204a01ff4a7f0174760a45afa2644ee36
dwj <dwj2930@163.com>
2019-02-07 19:24:41 +0800

    可以在地图上显示小区名称和价格。聚合点和覆盖点混合展示，图显得有点凌乱。

--- evaluate/views.py
+++ evaluate/views.py
@@ -53,13 +53,14 @@ def average(request):
 
 def averageQuery(request):
     queryAttribute = request.path.split('/')[3]
+    import pymysql
+    conn = pymysql.connect(host='101.132.154.2', user='housing', passwd='housing', db='House_OnSale',
+                           charset='utf8')
     if queryAttribute == 'diskName':
         params = request.GET.keys()
         if params.__contains__('diskName') and params.__contains__('month'):
             diskName = request.GET['diskName']
             (year, m) = request.GET['month'].split('-')
-            import pymysql
-            conn = pymysql.connect(host='101.132.154.2', user='housing', passwd='housing', db='House_OnSale', charset='utf8')
             cursor = conn.cursor()
             res = {}
             for mons in range(1,4):  # 暂时先查询01-03月的数据，等数据库全部数据导入完成再改为range(1,13)
@@ -76,19 +77,17 @@ def averageQuery(request):
                     return JsonResponse({'status': "ok", 'month': m, 'diskName': diskName, 'data': 'no match'})
                 price = cursor.fetchone()[1]
                 res[month] = price
+            cursor.close()
             return JsonResponse({'status': "ok", 'month': m, 'diskName': diskName, 'data': res})
         else:
             return JsonResponse({'status': "params not fully specified"})
 
     elif queryAttribute == 'plate':     # 只能查看201901月后的板块/小区均价。之前的数据库中plate内容有丢失。
         params = request.GET.keys()
-        if params.__contains__('plate'):
+        if params.__contains__('plate') and params.__contains__('month'):
             plate = request.GET['plate']
             month = request.GET['month']
             res = {}
-            import pymysql
-            conn = pymysql.connect(host='101.132.154.2', user='housing', passwd='housing', db='House_OnSale',
-                                   charset='utf8')
             cursor = conn.cursor()
             try:
                 cursor.callproc("avg_plate_month", (plate, month) )
@@ -96,13 +95,45 @@ def averageQuery(request):
                 return JsonResponse({'status': 'ok', 'data': 'sql error'})
             avg_disks = cursor.fetchall()
             for avg_disk in avg_disks:
-                res[avg_disk[0]] = avg_disk[1]
+                res[avg_disk[0]] = (avg_disk[1],avg_disk[2])
             print(res)
+            cursor.close()
             return JsonResponse({'status': 'ok', 'data': res})
+        else:
+            return JsonResponse({'status': "params not fully specified"})
+
+    elif queryAttribute == 'allDisk':
+        params = request.GET.keys()
+        if params.__contains__('month'):
+            month = str(request.GET['month'])
+            if not tableExists(month):
+                return JsonResponse({'status': 'ok', 'month': month, 'data': 'month out of range'})
+            cursor = conn.cursor()
+            try:    # 存储过程'avg_allDisk_month'作用：查询指定月份内全市所有小区的平均每平米价格。若上一次统计为五天前，那么重新统计。
+                cursor.callproc('avg_allDisk_month', args=(month,))        # 存储过程运行完成，数据没有commit，当前conn会话仍持有锁，因此insert没有在数据库中永久生效，所以在navicat中不可见新的数据。
+                data = cursor.fetchall()
+                conn.commit()   # 必须写commit，用来释放锁，否则insert不会在数据库中最终生效。
+            except pymysql.err.ProgrammingError:
+                return JsonResponse({'status': 'ok', 'data': 'sql error'})
+            return JsonResponse({'status': 'ok', 'month': month, 'data': data})
         else:
             return JsonResponse({'status': "params not fully specified"})
     else:
         return JsonResponse({'status': "invalid api access"})
+
+def tableExists(tableName):
+    import pymysql
+    conn_table = pymysql.connect(host='101.132.154.2', user='housing', passwd='housing', db='House_OnSale',
+                           charset='utf8')
+    cursor_table = conn_table.cursor()
+    sql = "show tables like '" + tableName + "';"
+    cursor_table.execute(sql)
+    match = cursor_table.rowcount
+    cursor_table.close()
+    conn_table.close()
+    if match != 0:
+        return True
+    return False
 
 def baseQuery(request):
     queryAttribute = request.path.split('/')[3]
